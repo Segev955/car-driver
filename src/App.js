c811835e653dfdab9a3ed46b004b63d5e8ccd924
segev955 <segev95@gmail.com>
2024-01-21 17:23:18 +0200

    Table(fake data)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from "react";
+import { useTable } from "react-table";
+import fakeData from "./data.json";
 
 function App() {
+  const data = React.useMemo(() => fakeData, [])
+  const columns = React.useMemo(() => [
+    {
+      Header: "Time Stamp",
+      accessor: "timestamp",
+    },
+    {
+      Header: "Vehicle Speed",
+      accessor: "vehicleSpeed",
+    },
+    {
+      Header: "Engine RPM",
+      accessor: "engineRPM",
+    },
+    {
+      Header: "Throttle Position",
+      accessor: "throttlePosition",
+    },
+    {
+      Header: "Fuel Level",
+      accessor: "fuelLevel",
+    },
+    {
+      Header: "Coolant Temperature",
+      accessor: "coolantTemperature",
+    },
+    {
+      Header: "Odometer Reading",
+      accessor: "odometerReading",
+    },
+    {
+      Header: "Brake Status",
+      accessor: "brakeStatus",
+    },
+    {
+      Header: "Gear Position",
+      accessor: "gearPosition",
+    },
+    {
+      Header: "Accelerator Position",
+      accessor: "acceleratorPosition",
+    },
+    {
+      Header: "VIN",
+      accessor: "VIN",
+    },
+  ],
+  []
+);
+
+  const {getTableProps, getTableBodyProps, headerGroups, rows, prepareRow} = useTable({columns, data});
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <table {...getTableProps()}>
+          <thead>
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <th {...column.getHeaderProps()}>
+                    {column.render("Header")}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {rows.map((row) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell) => (
+                    <td {...cell.getCellProps()}> {cell.render("Cell")} </td>
+                  ))}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
