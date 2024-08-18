34f8ff8141ca43c349507d54c4605e89e30c6b5c
Segev <76211433+Segev955@users.noreply.github.com>
2024-08-18 19:07:12 +0300

    notification

---
+++ notification-backend/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const admin = require("firebase-admin");
+const cors = require("cors"); // Import the cors package
+
+
+// Load your service account key JSON file
+const serviceAccount = require("./car-driver-bc91f-firebase-adminsdk-xhkyn-214c09b623.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://car-driver-bc91f-default-rtdb.asia-southeast1.firebasedatabase.app",
+});
+
+const app = express();
+app.use(cors()); 
+app.use(bodyParser.json());
+
+app.post("/send-notification", async (req, res) => {
+  const { token, title, body } = req.body;
+
+  const message = {
+    token: token,
+    notification: {
+      title: title,
+      body: body,
+    },
+  };
+
+  try {
+    const response = await admin.messaging().send(message);
+    console.log("Notification sent successfully:", response);
+    res.status(200).send("Notification sent successfully!");
+  } catch (error) {
+    console.error("Error sending notification:", error);
+    res.status(500).send("Error sending notification");
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
